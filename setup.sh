7f526dd935e4f0f0ae41e4d6d4fe20646343fb0e
Saul Arciniega <zaul_ae@hotmail.com>
2022-12-21 20:00:49 -0500

    Setup updated

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-git clone https://github.com/SaulArciniegaEsparza/presiones-cdmx.git
-cd presiones-cdmx
+sudo apt-get update
 sudo apt-get install python3-virtualenv
-virtualenv -q -p /usr/bin/python3 venv
+python3 -m virtualenv -q -p /usr/bin/python3 venv
 source ./venv/bin/activate
 ./venv/bin/pip install -r requirements.txt
 ./venv/bin/streamlit run 1_Inicio.py
